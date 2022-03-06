14688a8753c8bc1fd8446b6c380b11bb5485ccb2
Ricardo Ruiz <ruizmunricardo@gmail.com>
2022-03-07 00:57:09 +0100

    Genarating fox images randomly with the ranndom fox API

--- src/index.js
+++ src/index.js
@@ -3,4 +3,35 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+
+const createImageNode = () => {
+
+    const random = Math.floor(Math.random() * 123) ;
+
+    const container = document.createElement('div')
+    container.className = 'p-4';
+
+    const image = document.createElement('img');
+    image.className = "mx-auto";
+    image.width = '320'
+    image.src = `https://randomfox.ca/images/${random}.jpg`;
+
+    container.appendChild(image);
+
+    return container;
+};
+
+const addImageNode = () => {
+    const newImage = createImageNode();
+    const mountNode = document.querySelector('#images')
+    mountNode.appendChild(newImage);
+}
+
+
+const generateButton = document.querySelector('#generate');
+
+generateButton.addEventListener('click', addImageNode);
+
+
+
+
